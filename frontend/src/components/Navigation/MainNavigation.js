8fac791947aa5fd944d90c4f5f04968aa6492148
Roohi Singh <singhroohi184@gmail.com>
2020-08-06 19:40:48 -0400

    updated
    updated

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import AuthContext from "../../context/auth-context";
 
+import AuthContext from "../../context/auth-context";
 import "./MainNavigation.css";
 
 const mainNavigation = (props) => (
@@ -23,9 +23,14 @@ const mainNavigation = (props) => (
                 <NavLink to="/events">Events</NavLink>
               </li>
               {context.token && (
-                <li>
-                  <NavLink to="/bookings">Bookings</NavLink>
-                </li>
+                <React.Fragment>
+                  <li>
+                    <NavLink to="/bookings">Bookings</NavLink>
+                  </li>
+                  <li>
+                    <button onClick={context.logout}>Logout</button>
+                  </li>
+                </React.Fragment>
               )}
             </ul>
           </nav>
