7ac09a498b201df9ea9021183b1ecde4ffd7a05c
Roohi Singh <singhroohi184@gmail.com>
2020-08-06 22:38:41 -0400

    updated
    updated

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Spinner from '../components/Spinner/Spinner';
-import AuthContext from '../context/auth-context';
-import BookingList from '../components/Bookings/BookingList/BookingList';
+import Spinner from "../components/Spinner/Spinner";
+import AuthContext from "../context/auth-context";
+import BookingList from "../components/Bookings/BookingList/BookingList";
+import BookingsChart from "../components/Bookings/BookingsChart/BookingsChart";
+import BookingsControls from "../components/Bookings/BookingsControls/BookingsControls";
 
 class BookingsPage extends Component {
   state = {
     isLoading: false,
-    bookings: []
+    bookings: [],
+    outputType: "list",
   };
 
   static contextType = AuthContext;
@@ -28,37 +31,38 @@ class BookingsPage extends Component {
                _id
                title
                date
+               price
              }
             }
           }
-        `
+        `,
     };
 
-    fetch('http://localhost:8080/graphql', {
-      method: 'POST',
+    fetch("http://localhost:8080/graphql", {
+      method: "POST",
       body: JSON.stringify(requestBody),
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + this.context.token
-      }
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + this.context.token,
+      },
     })
-      .then(res => {
+      .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Failed!');
+          throw new Error("Failed!");
         }
         return res.json();
       })
-      .then(resData => {
+      .then((resData) => {
         const bookings = resData.data.bookings;
         this.setState({ bookings: bookings, isLoading: false });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         this.setState({ isLoading: false });
       });
   };
 
-  deleteBookingHandler = bookingId => {
+  deleteBookingHandler = (bookingId) => {
     this.setState({ isLoading: true });
     const requestBody = {
       query: `
@@ -70,51 +74,69 @@ class BookingsPage extends Component {
           }
         `,
       variables: {
-        id: bookingId
-      }
+        id: bookingId,
+      },
     };
 
-    fetch('http://localhost:8080/graphql', {
-      method: 'POST',
+    fetch("http://localhost:8080/graphql", {
+      method: "POST",
       body: JSON.stringify(requestBody),
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + this.context.token
-      }
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + this.context.token,
+      },
     })
-      .then(res => {
+      .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Failed!');
+          throw new Error("Failed!");
         }
         return res.json();
       })
-      .then(resData => {
-        this.setState(prevState => {
-          const updatedBookings = prevState.bookings.filter(booking => {
+      .then((resData) => {
+        this.setState((prevState) => {
+          const updatedBookings = prevState.bookings.filter((booking) => {
             return booking._id !== bookingId;
           });
           return { bookings: updatedBookings, isLoading: false };
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         this.setState({ isLoading: false });
       });
   };
 
+  changeOutputTypeHandler = (outputType) => {
+    if (outputType === "list") {
+      this.setState({ outputType: "list" });
+    } else {
+      this.setState({ outputType: "chart" });
+    }
+  };
+
   render() {
-    return (
-      <React.Fragment>
-        {this.state.isLoading ? (
-          <Spinner />
-        ) : (
-          <BookingList
-            bookings={this.state.bookings}
-            onDelete={this.deleteBookingHandler}
+    let content = <Spinner />;
+    if (!this.state.isLoading) {
+      content = (
+        <React.Fragment>
+          <BookingsControls
+            activeOutputType={this.state.outputType}
+            onChange={this.changeOutputTypeHandler}
           />
-        )}
-      </React.Fragment>
-    );
+          <div>
+            {this.state.outputType === "list" ? (
+              <BookingList
+                bookings={this.state.bookings}
+                onDelete={this.deleteBookingHandler}
+              />
+            ) : (
+              <BookingsChart bookings={this.state.bookings} />
+            )}
+          </div>
+        </React.Fragment>
+      );
+    }
+    return <React.Fragment>{content}</React.Fragment>;
   }
 }
 
